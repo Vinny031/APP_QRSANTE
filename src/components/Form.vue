0727adc543ff678569c2507cd125639c3522ca34
julienop <julienoperon@gmail.com>
2025-09-24 15:31:21 +0200

    ajout scanner.vue et modif balises

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,146 +3,128 @@
     <Header />
     <h2>Bienvenue sur votre espace QR Santé</h2>
 
-    <h3>Données personnelles</h3>
-    <div class="form">
-      <div class="form-row">
-        <label for="birthDate">Date de naissance</label>
-        <input
-          type="text"
-          id="birthDate"
-          v-model="patientData.birthDate"
-          placeholder="JJ/MM/AAAA"
-          @input="formatDate"
-        />
-      </div>
+        <div class="form-container">
+            <h3>Données personnelles</h3>
+            <div class="card">
+                <div class="form-row">
+                    <label for="birthDate">Date de naissance</label>
+                    <input
+                        type="text"
+                        id="birthDate"
+                        v-model="patientData.birthDate"
+                        placeholder="JJ/MM/AAAA"
+                        @input="formatDate"
+                    />
+                </div>
+                <div class="form-row">
+                    <label for="birthplace">Lieu de naissance</label>
+                    <input
+                        type="text"
+                        id="birthplace"
+                        v-model="patientData.birthplace"
+                        @input="patientData.birthplace = patientData.birthplace.toUpperCase()"
+                    />
+                </div>
+                <div class="form-row">
+                    <label for="birthDepartment">Département de naissance</label>
+                    <select id="birthDepartment" v-model="patientData.birthDepartment">
+                        <option disabled value="">-- Sélectionnez un département --</option>
+                        <option v-for="dep in departements" :key="dep.id" :value="dep.id">
+                            {{ dep.id }} - {{ dep.name }}
+                        </option>
+                    </select>
+                </div>
+                <div class="form-row">
+                    <label for="currentAddress">Adresse actuelle</label>
+                    <input type="text" id="currentAddress" v-model="patientData.currentAddress" />
+                </div>
+            </div>
+    
+            <h3>Carnet médical</h3>
+            <div class="card">
+                <h4>Les essentielles :</h4>
+                <div class="form-row">
+                    <label for="hasPacemaker">Pacemaker ?</label>
+                    <div class="options">
+                        <input type="radio" id="pacemaker-yes" name="pacemaker" :value="true" v-model="patientData.hasPacemaker" />
+                        <label for="pacemaker-yes">Oui</label>
+                        <input type="radio" id="pacemaker-no" name="pacemaker" :value="false" v-model="patientData.hasPacemaker" />
+                        <label for="pacemaker-no">Non</label>
+                    </div>
+                </div>
+                
+                <div class="form-row">
+                    <label for="organDonation">Don d'organes ?</label>
+                    <div class="options">
+                        <input type="radio" id="organ-yes" name="organDonation" :value="true" v-model="patientData.organDonation" />
+                        <label for="organ-yes">Oui</label>
+                        <input type="radio" id="organ-no" name="organDonation" :value="false" v-model="patientData.organDonation" />
+                        <label for="organ-no">Non</label>
+                    </div>
+                </div>
 
-      <div class="form-row">
-        <label for="birthplace">Lieu de naissance</label>
-        <input
-          type="text"
-          id="birthplace"
-          v-model="patientData.birthplace"
-          @input="patientData.birthplace = patientData.birthplace.toUpperCase()"
-        />
-      </div>
+                <div class="form-row">
+                    <label for="allergiesKnown">Allergies connues ?</label>
+                    <div class="options">
+                        <input type="radio" id="allergies-yes" name="allergies" :value="true" v-model="patientData.allergiesKnown" />
+                        <label for="allergies-yes">Oui</label>
+                        <input type="radio" id="allergies-no" name="allergies" :value="false" v-model="patientData.allergiesKnown" />
+                        <label for="allergies-no">Non</label>
+                    </div>
+                </div>
 
-      <div class="form-row">
-        <label for="birthDepartment">Département de naissance</label>
-        <select id="birthDepartment" v-model="patientData.birthDepartment">
-          <option disabled value="">-- Sélectionnez un département --</option>
-          <option v-for="dep in departements" :key="dep.id" :value="dep.id">
-            {{ dep.id }} - {{ dep.name }}
-          </option>
-        </select>
-      </div>
+                <div class="form-row">
+                    <label for="allergies">Allergies</label>
+                    <input type="text" id="allergies" v-model="patientData.allergies" />
+                </div>
 
-      <div class="form-row">
-        <label for="currentAddress">Adresse actuelle</label>
-        <input type="text" id="currentAddress" v-model="patientData.currentAddress" />
-      </div>
-    </div>
-
-    <h3>Carnet médical</h3>
-    <div class="form">
-      <!-- Essentielles -->
-      <div class="card">
-        <h4>Les essentielles :</h4>
-
-        <div class="form-row">
-          <label>Pacemaker ?</label>
-          <div class="radio-group">
-            <label>
-              <span>Oui</span>
-              <input type="radio" name="pacemaker" :value="true" v-model="patientData.hasPacemaker" />
-            </label>
-            <label>
-              <span>Non</span>
-              <input type="radio" name="pacemaker" :value="false" v-model="patientData.hasPacemaker" />
-            </label>
-          </div>
-        </div>
-
-        <div class="form-row">
-          <label>Don d'organes ?</label>
-          <div class="radio-group">
-            <label>
-              <span>Oui</span>
-              <input type="radio" name="organDonation" :value="true" v-model="patientData.organDonation" />
-            </label>
-            <label>
-              <span>Non</span>
-              <input type="radio" name="organDonation" :value="false" v-model="patientData.organDonation" />
-            </label>
-          </div>
-        </div>
-
-        <div class="form-row">
-          <label>Allergies connues ?</label>
-          <div class="radio-group">
-            <label>
-              <span>Oui</span>
-              <input type="radio" name="allergiesKnown" :value="true" v-model="patientData.allergiesKnown" />
-            </label>
-            <label>
-              <span>Non</span>
-              <input type="radio" name="allergiesKnown" :value="false" v-model="patientData.allergiesKnown" />
-            </label>
-          </div>
-        </div>
-
-        <div class="form-row">
-          <label for="allergies">Allergies</label>
-          <input type="text" id="allergies" v-model="patientData.allergies" />
-        </div>
-
-        <div class="form-row">
-          <label for="bloodType">Groupe sanguin</label>
-          <select v-model="patientData.bloodType" id="bloodType">
-            <option value="" disabled>Sélectionner un groupe</option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
-        </div>
-      </div>
-
-      <!-- Données médicales -->
-      <div class="card">
-        <h4>Données médicales :</h4>
-        <div class="form-row">
-          <label for="medicalEquipment">Matériel médical</label>
-          <input type="text" id="medicalEquipment" v-model="patientData.medicalEquipment" />
-        </div>
-        <div class="form-row">
-          <label for="chronicDiseases">Maladies chroniques</label>
-          <input type="text" id="chronicDiseases" v-model="patientData.chronicDiseases" />
-        </div>
-        <div class="form-row">
-          <label for="medicalHistory">Antécédents médicaux</label>
-          <input type="text" id="medicalHistory" v-model="patientData.medicalHistory" />
-        </div>
-        <div class="form-row">
-          <label for="currentTreatments">Traitements en cours</label>
-          <input type="text" id="currentTreatments" v-model="patientData.currentTreatments" />
-        </div>
-        <div class="form-row">
-          <label for="medicalExams">Examens médicaux</label>
-          <input type="text" id="medicalExams" v-model="patientData.medicalExams" />
-        </div>
-        <div class="form-row">
-          <label for="advanceDirectives">Directives anticipées</label>
-          <input type="text" id="advanceDirectives" v-model="patientData.advanceDirectives" />
-        </div>
-        <div class="form-row">
-          <label for="vaccinations">Vaccinations</label>
-          <input type="text" id="vaccinations" v-model="patientData.vaccinations" />
-        </div>
-      </div>
+                <div class="form-row">
+                    <label for="Groupe sanguin">Groupe sanguin</label>
+                    <select v-model="patientData.bloodType" id="Groupe sanguin">
+                        <option value="" disabled selected>Sélectionner un groupe</option>
+                        <option value="A+">A+</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B-">B-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
+                        <option value="O+">O+</option>
+                        <option value="O-">O-</option>
+                    </select>
+                </div>
+            </div>
+            
+            <div class="card">
+                <h4>Données médicales :</h4>
+                    <div class="form-row">
+                        <label for="medicalEquipment">Matériel médical (PTG, PTH, PHC...)</label>
+                        <input type="text" id="medicalEquipment" v-model="patientData.medicalEquipment" />
+                    </div>
+                    <div class="form-row">
+                        <label for="chronicDiseases">Maladies chroniques</label>
+                        <input type="text" id="chronicDiseases" v-model="patientData.chronicDiseases" />
+                    </div>
+                    <div class="form-row">
+                        <label for="medicalHistory">Antécédents médicaux</label>
+                        <input type="text" id="medicalHistory" v-model="patientData.medicalHistory" />
+                    </div>
+                    <div class="form-row">
+                        <label for="currentTreatments">Traitements en cours</label>
+                        <input type="text" id="currentTreatments" v-model="patientData.currentTreatments" />
+                    </div>
+                    <div class="form-row">
+                        <label for="medicalExams">Examens médicaux</label>
+                        <input type="text" id="medicalExams" v-model="patientData.medicalExams" />
+                    </div>
+                    <div class="form-row">
+                        <label for="advanceDirectives">Directives anticipées</label>
+                        <input type="text" id="advanceDirectives" v-model="patientData.advanceDirectives" />
+                    </div>
+                    <div class="form-row">
+                        <label for="vaccinations">Vaccinations</label>
+                        <input type="text" id="vaccinations" v-model="patientData.vaccinations" />
+                    </div>
+            </div>
 
       <!-- Contacts -->
       <div class="card">
@@ -224,7 +206,7 @@ function formatDate(e) {
 </script>
 
 <style scoped>
-.form, .form *, .card {
+.form-container, .form-row, .card {
     box-sizing: border-box;
 }
 
@@ -243,55 +225,57 @@ h2, h3 {
     font-size: 18px;
 }
 
-.form {
+.form-container {
     display: flex;
     flex-direction: column;
-    gap: 15px;
+    gap: 20px;
     align-items: flex-start;
 }
 
 .form-row {
     display: flex;
-    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
+    gap: 15px; /* Ajout d'un écart pour les éléments */
 }
 
 .form-row label {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
+    flex-basis: 40%;
+    flex-shrink: 0;
+    font-weight: bold;
+    color: #555;
+    text-align: left;
 }
 
 .form-row input,
 .form-row select {
-    width: 100%;
+    flex-grow: 1;
     padding: 12px;
     font-size: 14px;
     border-radius: 8px;
     border: 1px solid #ddd;
 }
 
-.radio-group {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-}
-
-.radio-group label {
+.options {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    gap: 8px;
-    cursor: pointer;
+    gap: 15px;
 }
 
+.options input[type="radio"] {
+    width: auto;
+    flex-grow: 0;
+}
+
+.options label {
+    font-weight: normal;
+}
 
 .card {
     border: 1px solid #ddd;
     border-radius: 10px;
     padding: 15px;
-    margin: 15px 0;
     background-color: #fff;
     box-shadow: 0 2px 5px rgba(0,0,0,0.1);
     width: 100%;
